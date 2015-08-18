7ff99d472a16a33f5b97bdbe4c01888a9c2af315
Luminous <867380699@qq.com>
2015-08-18 15:49:17 +0800

    realized!

--- G2015010210/Homework/app/src/main/java/com/geekband/luminous/homework/widget/MyGridView.java
+++ G2015010210/Homework/app/src/main/java/com/geekband/luminous/homework/widget/MyGridView.java
@@ -8,17 +8,22 @@ import android.widget.GridView;
  * a new GridView
  * Created by hisashieki on 15/8/14.
  */
+
 public class MyGridView extends GridView {
 
     public MyGridView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
 
+        super(context, attrs);
+
+        System.out.println("");
+    }
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
                 MeasureSpec.AT_MOST);
-        System.out.println(Integer.MAX_VALUE >> 2);
+
         super.onMeasure(widthMeasureSpec, expandSpec);
+
+        System.out.println();
     }
 }
