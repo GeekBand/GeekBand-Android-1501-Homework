d65b2785b55170750c86ae329440054960ed48d1
Luminous <867380699@qq.com>
2015-08-22 17:43:42 +0800

    3D and Color filter

---
+++ G2015010210/Homework/app/src/main/java/com/geekband/luminous/homework/widget/MyImageView.java
@@ -0,0 +1,31 @@
+package com.geekband.luminous.homework.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * aaa
+ * Created by hisashieki on 15/8/22.
+ */
+public class MyImageView extends View {
+
+    public MyImageView(Context context) {
+        super(context);
+    }
+
+    public MyImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}
